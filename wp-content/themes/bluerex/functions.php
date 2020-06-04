72bc03dc8cfcae0fd8106a971dd7588f7bedcb01
Marisha-tech <malayamarisha@yandex.ru>
2020-06-04 10:49:31 +0700

    Add function background

--- wp-content/themes/bluerex/functions.php
+++ wp-content/themes/bluerex/functions.php
@@ -199,3 +199,13 @@ function bluerex_debug($data)
 {
     echo '<pre>'. print_r($data, 1) . '</pre>';
 }
+
+function bluerex_get_background($field, $cat = null, $cover = true)
+{
+    if (get_field($field, $cat))
+    {
+        $add_style = $cover ? 'background-size: cover' : '';
+        return ' style="background: url('.get_field($field, $cat) . ') center no-repeat; '.$add_style . '"';
+    }
+    return null;
+}
