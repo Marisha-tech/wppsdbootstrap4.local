78221ec73528345837c43c9a2ebe84da0adf37d1
Marisha-tech <malayamarisha@yandex.ru>
2020-06-04 10:40:29 +0700

    Custom header

--- wp-content/themes/bluerex/header.php
+++ wp-content/themes/bluerex/header.php
@@ -1,59 +1,75 @@
-<?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package bluerex
- */
-
-?>
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<html lang="en">
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-
-	<?php wp_head(); ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--<style>
+        .preloader
+        {
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            z-index: 9999;
+            background-color: #fefefe;
+        }
+    </style>-->
+    <title>Hello, world!</title>
+    <?wp_head()?>
 </head>
+<body>
+<!--<div class="preloader d-flex align-items-center justify-content-center">
+    <div class="spinner-border text-danger" style="width: 10rem; height: 10rem;" role="status">
+        <span class="sr-only">Loading...</span>
+    </div>
+</div>-->
+<button class="scrollToTop"><i class="fas fa-angle-up"></i></button>
+<header class="main-header" <?echo bluerex_get_background('header_bg')?>>
+    <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand" href="<?echo home_url('/')?>">
+            <?$custom_logo = wp_get_attachment_image_src(get_theme_mod('custom_logo'));?>
+            <?if ($custom_logo):?>
+                <img src="<?echo $custom_logo[0]?>" alt="<?bloginfo('name')?>">
+            <?endif?>
+            <?bloginfo('name')?>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+        </button>
 
-<body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'bluerex' ); ?></a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <?wp_nav_menu(array(
+                'theme_location'  => 'header_menu',
+                'container'       => false,
+                'menu_class'      => 'navbar-nav ml-auto',
+            ))?>
+        </div>
+    </nav>
+    <div class="main-header-text">
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$bluerex_description = get_bloginfo( 'description', 'display' );
-			if ( $bluerex_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $bluerex_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+        <?if(get_field('header1')):?>
+            <h3><?the_field('header1')?></h3>
+        <?endif?>
+        <?if(get_field('header2')):?>
+            <h4><?the_field('header2')?></h4>
+        <?endif?>
+        <?if(get_field('header_text')):?>
+            <p><?the_field('header_text')?></p>
+        <?endif?>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'bluerex' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+        <div class="main-header-buttons">
+
+            <?if(get_field('header_btn1')):?>
+                <?$link = get_field('header_btn1');?>
+                <a href="<?echo esc_url( $link['url']); ?>" class="btn btn-pink rounded-pill"><?echo esc_html( $link['title']);?></a>
+            <?endif?>
+            <?if(get_field('header_btn2')):?>
+                <?$link = get_field('header_btn2');?>
+                <a href="<?echo esc_url( $link['url']); ?>" class="btn btn-violet rounded-pill"><?echo esc_html( $link['title']);?></a>
+            <?endif?>
+        </div>
+    </div>
+    <!-- /.main-header-text -->
+</header>
