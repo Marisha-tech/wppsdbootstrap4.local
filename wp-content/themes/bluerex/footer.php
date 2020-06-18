58db038984db0c87ebe2ab9e317d36cb25446cdc
Marisha-tech <malayamarisha@yandex.ru>
2020-06-18 13:40:28 +0700

    Футер на других страницах

--- wp-content/themes/bluerex/footer.php
+++ wp-content/themes/bluerex/footer.php
@@ -3,63 +3,15 @@
         <div class="row">
             <div class="col-md-6">
                 <div class="row">
-                    <div class="col-6">
-                        <h5>Company</h5>
-                        <ul>
-                            <li><a href="#">Company</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Approach</a></li>
-                            <li><a href="#">Sustainability</a></li>
-                            <li><a href="#">Careers</a></li>
-                            <li><a href="#">News</a></li>
-                        </ul>
-                    </div>
-                    <!-- /.col-6 -->
-                    <div class="col-6">
-                        <h5>Company</h5>
-                        <ul>
-                            <li><a href="#">Company</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Approach</a></li>
-                            <li><a href="#">Sustainability</a></li>
-                            <li><a href="#">Careers</a></li>
-                            <li><a href="#">News</a></li>
-                        </ul>
-                    </div>
-                    <!-- /.col-6 -->
-                    <div class="col-6">
-                        <h5>Company</h5>
-                        <ul>
-                            <li><a href="#">Company</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Approach</a></li>
-                            <li><a href="#">Sustainability</a></li>
-                            <li><a href="#">Careers</a></li>
-                            <li><a href="#">News</a></li>
-                        </ul>
-                    </div>
-                    <!-- /.col-6 -->
-                    <div class="col-6">
-                        <h5>Company</h5>
-                        <ul>
-                            <li><a href="#">Company</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Approach</a></li>
-                            <li><a href="#">Sustainability</a></li>
-                            <li><a href="#">Careers</a></li>
-                            <li><a href="#">News</a></li>
-                        </ul>
-                    </div>
-                    <!-- /.col-6 -->
+                   <? dynamic_sidebar('sidebar-footer1')?>
                 </div>
             </div>
             <!-- /.col-md-6 -->
             <div class="col-md-6 footer-images">
                 <div class="row">
-                    <div class="col-6"><img src="<?echo bloginfo(template_url);?>/assets/img/footer_1.png" alt=""></div>
-                    <div class="col-6"><img src="<?echo bloginfo(template_url);?>/assets/img/footer_2.png" alt=""></div>
-                    <div class="col-6"><img src="<?echo bloginfo(template_url);?>/assets/img/footer_3.png" alt=""></div>
-                    <div class="col-6"><img src="<?echo bloginfo(template_url);?>/assets/img/footer_4.png" alt=""></div>
+                    <div class="col-md-12">
+                        <? dynamic_sidebar('sidebar-footer2')?>
+                    </div>
                 </div>
             </div>
             <!-- /.col-md-6 -->
