1a23a63ed348c88e71b85607efbd17abfbda9106
Marisha-tech <malayamarisha@yandex.ru>
2020-06-18 13:38:10 +0700

    Шаблон записи

--- wp-content/themes/bluerex/page.php
+++ wp-content/themes/bluerex/page.php
@@ -1,38 +1,35 @@
-<?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package bluerex
- */
+<?get_header()?>
+<section class="section-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
 
-get_header();
-?>
-
-	<main id="primary" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+                <? if (have_posts()) : ?>
+                    <? while (have_posts()) : the_post(); ?>
+                        <article class="article-preview">
+                            <h2><?the_title()?></h2>
+                            <p><span class="article-date"><i class="far fa-calendar-alt"></i><?the_time('d.m.Y')?></span></p>
+                            <div class="article-excerpt">
+                                <?if (has_post_thumbnail()): ?>
+                                    <div class="bluerex-thumb">
+                                        <a href="<?the_permalink() ?>">
+                                            <?the_post_thumbnail('thumbnail', array('class'=>'thumb')) ?>
+                                        </a>
+                                    </div>
+                                <?endif;?>
+                                <?the_content('')?>
+                            </div>
+                        </article>
+                        <!-- /.article-preview -->
+                    <? endwhile; ?>
+                <?endif;?>
+            </div>
+            <!-- /.col-md-8 -->
+            <?get_sidebar()?>
+        </div>
+        <!-- /.col-md-4 -->
+    </div>
+    </div>
+</section>
+<!-- /.section-content -->
+<?get_footer()?>
